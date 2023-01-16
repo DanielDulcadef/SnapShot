75017e3cd4c53f2a8bf659855ad63bc12461343f
DanielDulcadef <dulcalito3@gmail.com>
2023-01-16 17:27:51 +0100

    Finish

--- src/App.js
+++ src/App.js
@@ -12,28 +12,29 @@ function App() {
 
   useEffect(() => {
     async function ax() {
-      if (imp !== "") {
-        await axios
-          .get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${imp}&format=json&nojsoncallback=1`)
-          .then((response) => {
-            let dat = response.data.photos.photo;
-            setImage(dat)
-          })
-      }
+
+      await axios
+        .get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${imp}&format=json&nojsoncallback=1`)
+        .then((response) => {
+          let dat = response.data.photos.photo;
+          setImage(dat)
+        })
+
     }
-    ax()
+    if (imp !== "") {
+      ax()
+    }
   }, [imp])
-  const imgcard = image.map(img1 => (<img key={img1.id} className='images' alt='imagenes' src={`https://live.staticflickr.com/${img1.server}/${img1.id}_${img1.secret}.jpg`}></img>))
   return (
     <div className="App">
       <div className='container'>
         <Tittle />
         <SearchPhotos search1={setImp} />
-        <Buttons buttons={setImp} />
+        <Buttons buttons={setImp} palabras={["Mountain", "Birds","Cars"]} />
       </div>
       <div><h2> {imp} Pictures</h2></div>
       <div>
-        {imgcard}
+        {image.map(img1 => (<img key={img1.id} className='images' alt='imagenes' src={`https://live.staticflickr.com/${img1.server}/${img1.id}_${img1.secret}.jpg`}></img>))}
       </div>
     </div>
 
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
-function Buttons({ buttons }) {
+function Buttons({ buttons, palabras }) {
 
   function btns(e) {
     buttons(e.target.value)
   }
   return (
     <div id="but">
-      <button className='btn' value={"Mountain"} onClick={btns}>Mountain</button>
-      <button className='btn' value={"Beaches"} onClick={btns}>Beaches</button>
-      <button className='btn' value={"Birds"} onClick={btns}>Birds</button>
-      <button className='btn' value={"Food"} onClick={btns}>Food</button>
+      {
+        palabras.map(texto => {
+          return (
+            <button className='btn' value={texto} onClick={btns}>{texto}</button>
+          )
+        })
+      }
+
     </div>
   )
 }
